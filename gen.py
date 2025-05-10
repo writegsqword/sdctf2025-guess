ddaecc6223366c7a5e052e3dae219c0a4f3c442b
a <arw007@ucsd.edu>
2025-05-09 21:46:38 -0700

    thing

--- gen.py
+++ gen.py
@@ -10,8 +10,8 @@ def write_file(fname, content):
     #with open(fname, "w") as f:
     #    f.write(content)
     zip_file.writestr(fname, content)
-lim = 10000000
-for i in range(0, lim, 1000):
+lim = 50000
+for i in range(0, lim, 50):
     write_file(f"{i}.txt", str(i))
 
 
