9848ba0e4991ce3914fcfb257c57a2bebc2c302a
a <arw007@ucsd.edu>
2025-05-09 21:43:02 -0700

    thing

--- gen.py
+++ gen.py
@@ -1,8 +1,18 @@
 import os
+import zipfile
 
 
+
+
+zip_file = zipfile.ZipFile("guesses.zip", "w")
+
+def write_file(fname, content):
+    #with open(fname, "w") as f:
+    #    f.write(content)
+    zip_file.writestr(fname, content)
 lim = 10000000
-for i in range(0, lim, 100000):
-    with open(f"{i}.txt", "w") as f:
-        f.write(str(i))
+for i in range(0, lim, 1000):
+    write_file(f"{i}.txt", str(i))
 
+
+zip_file.close()
