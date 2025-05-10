abc3e5d7cdf98cb3333ee6f1013518b33f35e36d
a <arw007@ucsd.edu>
2025-05-10 07:01:22 -0700

    thing

--- gen.py
+++ gen.py
@@ -1,18 +1,22 @@
 import os
 import zipfile
+import sys
 
-
-
-
+try:
+    os.remove('guesses.zip')
+except:
+    pass
 zip_file = zipfile.ZipFile("guesses.zip", "w")
 
 def write_file(fname, content):
     #with open(fname, "w") as f:
     #    f.write(content)
     zip_file.writestr(fname, content)
-lim = 125000000 * 4
+
+chunk = int(sys.argv[1])
+lim = 125000000 * 2
 #ideal step size is 1136 but i dont want to deal with round errors
-for i in range(-lim, lim, 2000):
+for i in range(lim * chunk, lim * (chunk + 1), 2000):
     write_file(f"{i}.txt", str(i))
 
 
